f95be501a26ad3a7620d4c2b696d1c101699f9d9
钟科 <edzhong3020@sina.com>
2018-05-27 16:16:57 +0800

    完善登录功能
    完善登录功能

--- mysite/views.py
+++ mysite/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render,HttpResponse,redirect
 from django.contrib.auth.views import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
@@ -9,7 +9,7 @@ from .forms import LoginForms
 def index(request):
     context = {}
     context['login_forms'] = LoginForms()
-    return render(request,"index1.html",context)
+    return render(request,"index.html",context)
 
 def register(request):
     return render(request,"register.html")
@@ -18,7 +18,7 @@ def tours(request):
     return render(request,"tours.html")
 
 def hots(request):
-    return render(request,"index.html")
+    return render(request,"hots.html")
 
 def blogs(request):
     return render(request,"blogs.html")
@@ -37,11 +37,11 @@ def login_user(request):
         print(user)
         if user is None:
             print("账号或密码错误")
-            return HttpResponse("fail",content_type='application/text')
+            return HttpResponse("fail")
         login(request,user)
         print("登入用户")
-        return HttpResponse("success",content_type='application/text')
-    return HttpResponse("fail",content_type='application/text')
+        return HttpResponse("success")
+    return HttpResponse("fail")
 
 @login_required(login_url="/home/")
 def logout_user(request):
