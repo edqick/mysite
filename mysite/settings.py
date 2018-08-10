bf94aaf79b0ceb2fdc32f6fe50ba83c3a5890ff8
钟科 <edzhong3020@sina.com>
2018-08-10 20:21:08 +0800

    init code

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,12 +76,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'lushi',
-        'USER': 'root',
-        'PASSWORD': '123456',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'NAME': 'lushi',
+        # 'USER': 'root',
+        # 'PASSWORD': '123456',
+        # 'HOST': '127.0.0.1',
+        # 'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'mydatabase',
     }
 }
 
